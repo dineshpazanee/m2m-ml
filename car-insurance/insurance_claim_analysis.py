51654ee5b3b6c30479792cba3857616ef65df8b5
din <din@pazani-2.local>
2025-02-18 19:01:15 -0400

    Adding the insurance claim data set

--- car-insurance/insurance_claim_analysis.py
+++ car-insurance/insurance_claim_analysis.py
@@ -1,8 +1,18 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 #load the csv fine into pandas
-df = pd.read_csv('./car-insurance/car_insurance_claim.csv')
+pd.set_option('display.max_columns', None)
+df = pd.read_csv('./car-insurance/insurance_claims.csv')
 
-print(df.info())
+gender_counts = df['insured_sex'].value_counts()
+
+gender_counts.plot.pie(autopct='%1.1f%%', startangle=90, colors=['lightblue', 'lightpink'])
+
+# Add a title
+plt.title('Gender Distribution')
+
+# Display the pie chart
+plt.show()
