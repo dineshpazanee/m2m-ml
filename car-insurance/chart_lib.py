866fbd4cefe8e8e585400c9531056e435d7c2ebe
din <din@mac.home>
2025-02-18 22:32:48 -0400

    added the gender distribution analysis

--- car-insurance/chart_lib.py
+++ car-insurance/chart_lib.py
@@ -1,3 +1,15 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
+
+class Chart_lib:
+    
+    def __init__(self):
+        pass
+    
+    
+    def pie_chart(self, data, title):
+        data.plot.pie(autopct='%1.1f%%', startangle=90, colors=['lightblue', 'lightpink'])
+        plt.title(title)
+        plt.show()
--- car-insurance/insurance_claim_analysis.py
+++ car-insurance/insurance_claim_analysis.py
@@ -1,18 +1,16 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from chart_lib import Chart_lib
 
 
 #load the csv fine into pandas
 pd.set_option('display.max_columns', None)
 df = pd.read_csv('./car-insurance/insurance_claims.csv')
+clib = Chart_lib()
 
 gender_counts = df['insured_sex'].value_counts()
+clib.pie_chart(gender_counts, 'Gender Distribution')
 
-gender_counts.plot.pie(autopct='%1.1f%%', startangle=90, colors=['lightblue', 'lightpink'])
-
-# Add a title
-plt.title('Gender Distribution')
-
-# Display the pie chart
-plt.show()
+genuine_incident = df[df['fraud_reported'] == 'N']
+clib.pie_chart(genuine_incident['insured_sex'].value_counts(), 'Gender Distribution from true Incident')
